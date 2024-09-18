dddca65e4ec054d031942447279e49ab3a4f8f4c
sabiha-wasema <sabihawasemaa1244@gmail.com>
2024-09-18 13:48:37 +0600

    form design done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      <iframe
+        src="https://xodu4asn.forms.app/untitled-form" 
+        frameBorder="0"
+        style={{
+          width: '100vw',
+          minWidth: '100%',
+          height: '600px',
+          border: 'none',
+        }}
+        allowtransparency="true"
+        allowFullScreen
+        allow="geolocation; microphone; camera"
+      ></iframe>
+    </>
+  );
+}
+
+export default App;
